3cb01a28754cccfc99844f7013b0e379cbddbe3d
Jaysen Spurlock <jaysen.spurlock@mines.sdsmt.edu>
2016-03-20 18:57:10 -0700

    Builds!

--- app/src/main/java/com/mythos_softworks/charactr/CharacterSelectionActivity.java
+++ app/src/main/java/com/mythos_softworks/charactr/CharacterSelectionActivity.java
@@ -3,10 +3,12 @@ package com.mythos_softworks.charactr;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 
-public class CharacterSelectionActivity extends ActionBarActivity {
+public class CharacterSelectionActivity extends ActionBarActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character_selection);
     }
